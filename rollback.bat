9f8122d62f7d5e35804d3fb9e20dc14479e96c2a
valen <lastleaf404@gmail.com>
2019-08-27 05:56:39 -0400

    Add files via upload

---
+++ rollback.bat
@@ -0,0 +1,2 @@
+reg add HKCU\Software\Classes\AppX82a6gwre4fdg3bt635tn5ctqjf8msdd2\Shell\open\command /f /d ""
+reg add HKCU\Software\Classes\AppX82a6gwre4fdg3bt635tn5ctqjf8msdd2\Shell\open\command /f /v DelegateExecute /d {4ED3A719-CEA8-4BD9-910D-E252F997AFC2}
